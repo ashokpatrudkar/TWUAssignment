605cac21a1cb17c238da67e9ff6cb28d6109be9c
ashokpatrudkar <pashok@thoughtworks.com>
2017-07-15 10:17:34 +0530

    add fizzbuzz exercise solution

---
+++ src/FizzBuzzExercise.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by pashok on 7/15/17.
+ */
+public class FizzBuzzExercise {
+
+    public static void printFizzBuzz(){
+        for(int number=1; number<=100; number++){
+            if(number%3==0 && number%5==0)
+                System.out.println("FizzBuzz");
+            else {
+                if(number%3==0)
+                    System.out.println("Fizz");
+                else {
+                    if(number%5==0)
+                        System.out.println("Buzz");
+                    else
+                        System.out.println(number);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printFizzBuzz();
+
+    }
+}
