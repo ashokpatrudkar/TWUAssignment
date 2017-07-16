f0fb89143d575af078892ef7e2d61a0763ed966a
ashokpatrudkar <pashok@thoughtworks.com>
2017-07-16 16:37:37 +0530

    add efficient method for calculating prime factors

--- src/PrimeFactorExercise.java
+++ src/PrimeFactorExercise.java
@@ -1,28 +1,35 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Created by pashok on 7/15/17.
  */
 public class PrimeFactorExercise {
 
     public static void printPrimeFactors(int number){
-         for(int i=1;i<=number;i++){
-             if(isPrimeFactor(i) && number%i==0)
-                 System.out.println(i);
-         }
-    }
+        Set primeFactors = new HashSet();
 
-    private static boolean isPrimeFactor(int number){
-        if(number<=1)
-            return false;
-        for(int i=2;i<=(int)Math.sqrt(number);i++){
-            if(number%i==0)
-                return false;
+        while(number%2==0){
+            primeFactors.add(2);
+            number/=2;
         }
-        return true;
+
+        for(int i=3;i<=(int)Math.sqrt(number);i+=2){
+            while (number%i==0) {
+                primeFactors.add(i);
+                number /= i;
+            }
+        }
+
+        if (number>2)
+            primeFactors.add(number);
+        System.out.println("Prime factors are : "+primeFactors);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number");
         int number = scanner.nextInt();
         printPrimeFactors(number);
     }
